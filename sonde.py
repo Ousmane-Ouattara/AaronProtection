7f3c954b470d1b14c430443ec2dae6fa8f3a2551
Ousmane-Ouattara <ousmane.ouattara@lycee-jeanrostand.fr>
2025-11-29 11:02:44 +0000

    changement de page accueil/index

--- sonde.py
+++ sonde.py
@@ -3,16 +3,16 @@ import time
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-# === CONFIG ===
+# CONFIG
 URL = "https://aaronprotection.onrender.com"
 INTERVAL = 60  # en secondes
 LOG_FILE = "log_sonde.csv"
 
-# === Initialisation ===
+# Initialisation
 with open(LOG_FILE, "a") as f:
     f.write("date_heure;latence_ms;status_code\n")
 
-# === Boucle de ping ===
+# Boucle de ping
 print(f"[SONDE] Surveillance de {URL} chaque {INTERVAL}s...\n")
 
 while True:
